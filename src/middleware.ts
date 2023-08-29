4ce02955f9a0f45312b310a6e551f3577680ff00
janilson.serra <j.janilson12@gmail.com>
2023-08-29 15:56:33 -0300

    Adição do middleware para redirecionar da raiz para a rota /home

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+
+  // Como o fomulario é chamado na _app.tsx, ele não chama o getServerSidePros
+  
+  if (request.nextUrl.pathname == '/'){
+    return NextResponse.redirect(new URL('/home', request.url))
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
