791baad26f1590929a85c57fa6b90aca5977f8c9
janilson.serra <j.janilson12@gmail.com>
2023-09-15 15:16:59 -0300

    Adição do loading padrão da área do assistido.

---
+++ src/app/(painel-assistido)/loading.tsx
@@ -0,0 +1,25 @@
+import { Box, Container, Skeleton } from '@mui/material'
+
+export default function loading() {
+  return (
+    <Container component="main" maxWidth="lg" sx={{ mb: 4 }}>
+      <Box sx={{ pt: 0.5 }}>
+        <Skeleton />
+        <Skeleton height={400} />
+        <Skeleton />
+        <Skeleton height={118} />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton height={118} />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+        <Skeleton height={118} />
+        <Skeleton />
+        <Skeleton />
+      </Box>
+    </Container>
+  )
+}
