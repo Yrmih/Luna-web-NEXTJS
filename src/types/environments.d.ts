59f34b275e788a700b267ab05a2fe5495008b6d9
Kaleu Silva <kaleu.rodrigues@defensoria.es.def.br>
2023-11-01 12:38:24 -0300

    Resolução de conflito configuração env

---
+++ src/types/environments.d.ts
@@ -0,0 +1,10 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    THEME_COLOR_PRIMARY_MAIN: string
+    THEME_COLOR_SECONDARY_MAIN: string
+    THEME_COLOR_BACKGROUND_DEFAULT: string
+    THEME_COLOR_BACKGROUND_PAPER: string
+    TOKEN_SOLAR: string
+    ENDPOINT_SOLAR: string
+  }
+}
