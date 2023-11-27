d6eb58682da2d42ce2201495e100bf40164a252f
janilson.serra <j.janilson12@gmail.com>
2023-11-27 13:45:25 -0300

    Remoção das env de tema

--- src/types/environments.d.ts
+++ src/types/environments.d.ts
@@ -1,9 +1,5 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    NEXT_PUBLIC_THEME_COLOR_PRIMARY_MAIN: string
-    NEXT_PUBLIC_THEME_COLOR_SECONDARY_MAIN: string
-    NEXT_PUBLIC_THEME_COLOR_BACKGROUND_DEFAULT: string
-    NEXT_PUBLIC_THEME_COLOR_BACKGROUND_PAPER: string
     TOKEN_SOLAR: string
     ENDPOINT_SOLAR: string
   }
