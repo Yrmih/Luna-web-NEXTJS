ea6d8862892854e2f59c0e4f32f44965969ea5fe
janilson.serra <j.janilson12@gmail.com>
2024-01-17 12:10:59 -0300

    Corrigir erro na tela de atendimentos: Functions cannot be passed directly to Client Components unless you explicitly expose it by marking

--- src/app/(painel-assistido)/atendimentos/components/page/Atendimentos.tsx
+++ src/app/(painel-assistido)/atendimentos/components/page/Atendimentos.tsx
@@ -1,8 +1,8 @@
 // Third party
 import { Box, Paper, Stack } from '@mui/material'
 
-import { ContainerAtendimento } from '../ui/ContainerAtendimento'
 // Internal
+import { ContainerAtendimento } from '../ui/ContainerAtendimento'
 
 export function Atendimentos() {
   return (
@@ -22,39 +22,10 @@ export function Atendimentos() {
             m: 0,
           }}
         >
-          <Box
-            color="white"
-            fontWeight={600}
-            fontSize={'1.5rem'}
-            sx={{
-              backgroundColor: (theme) =>
-                theme.palette.mode === 'light'
-                  ? theme.palette.primary.main
-                  : '#171717',
-              paddingTop: '8vh',
-              paddingLeft: '5vh',
-              width: '100%',
-              height: '25vh',
-            }}
-          >
+          <Box color="white" fontWeight={600} fontSize={'1.5rem'}>
             Meus Atendimentos
           </Box>
-          <Stack
-            direction={'column'}
-            sx={{
-              backgroundColor: (theme) =>
-                theme.palette.mode === 'light' ? 'white' : '#1b1b1b',
-              padding: '2vh',
-              paddingBottom: '10vh',
-
-              alignItems: 'center',
-              boxShadow: 2,
-              borderRadius: '3vh',
-              marginTop: '-8vh',
-              marginBottom: '4vh',
-              width: '95%',
-            }}
-          >
+          <Stack direction={'column'}>
             <ContainerAtendimento props={{ tipoAtendimento: 'pre' }} />
             <ContainerAtendimento props={{ tipoAtendimento: 'atendimento' }} />
             <ContainerAtendimento props={{ tipoAtendimento: 'agendamento' }} />
