89cfee8b4376b517ef1d9ea65bb3e608caf9ce5b
janilson.serra <j.janilson12@gmail.com>
2024-01-22 08:25:48 -0300

    Remoção de comentários do middleware.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,3 @@
-// import { NextRequest, NextResponse } from 'next/server'
-// export async function middleware(request: NextRequest) {
-//   // Como o fomulario é chamado na _app.tsx, ele não chama o getServerSidePros
-//   if (request.nextUrl.pathname === '/') {
-//     return NextResponse.redirect(new URL('/login', request.url))
-//   }
-// }
 import { JWT } from 'next-auth/jwt'
 import {
   withAuth,
@@ -43,16 +36,3 @@ export default withAuth(middleware, callbackOptions)
 export const config = {
   matcher: '/((?!api|_next/static|_next/image|favicon.ico|login).*)',
 }
-
-// export const config = {
-//   matcher: [
-//     /*
-//      * Match all request paths except for the ones starting with:
-//      * - api (API routes)
-//      * - _next/static (static files)
-//      * - _next/image (image optimization files)
-//      * - favicon.ico (favicon file)
-//      */
-//     '/((?!api|_next/static|_next/image|favicon.ico).*)',
-//   ],
-// }
