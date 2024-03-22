33558cf6e381f31313e9e7259b701b5a4670e107
janilson.serra <j.janilson12@gmail.com>
2024-03-22 14:59:21 -0300

    Reestruturação do build para adição de testes no pipeline.

--- src/types/environments.d.ts
+++ src/types/environments.d.ts
@@ -1,7 +1,9 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
+    NODE_ENV: string
     TOKEN_SOLAR: string
     SOLAR_API: string
+    NEXT_OUTPUT: string
     NEXT_PUBLIC_DEFENSORIA_NOME: string
   }
 }
